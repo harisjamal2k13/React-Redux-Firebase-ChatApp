8d1d77730ab6c9af76b98c7cf92b850b5835d8b2
harisjamal2k13 <harisjamal2k13@gmail.com>
2018-02-01 20:50:52 +0500

    React+Redux+Firebase-ChatApp

---
+++ src/constants/firebase.js
@@ -0,0 +1,16 @@
+export default {
+	firebaseConfig: {
+		apiKey: "AIzaSyBkHaS5B9_gnEgM_O78QFsbVQWJRPgFILg",
+		authDomain: "redux-react-firebase-chatapp.firebaseapp.com",
+		databaseURL: "https://redux-react-firebase-chatapp.firebaseio.com",
+		projectId: "redux-react-firebase-chatapp",
+		storageBucket: "redux-react-firebase-chatapp.appspot.com",
+		messagingSenderId: "67253531472"
+	}
+};
+
+// apiKey: "AIzaSyB3JUC4AoKZ5CJ0B8zVH5W4NVZMzZGIQZ0",
+// authDomain: "reactreduxchatapp.firebaseapp.com",
+// databaseURL: "https://reactreduxchatapp.firebaseio.com",
+// storageBucket: "reactreduxchatapp.appspot.com",
+// messagingSenderId: "827164131302"
